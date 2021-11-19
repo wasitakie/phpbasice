28c9210d46eacc5825e02dc90942bdecf5ed4ea2
wasitakie <94669314+wasitakie@users.noreply.github.com>
2021-11-19 19:12:49 +0700

    variable
    exphp

---
+++ variable.php
@@ -0,0 +1,42 @@
+<?php
+    print("hello");
+    print("hello");
+    print("hello");
+    print("<br>");
+    echo("hello");
+    $name="sssss";
+    $age=33;
+    $relation=true;
+    echo($name);
+    define("province","bangkok");
+    echo provice;
+    echo("<br>");
+    define("provine","15000");
+    echo salary;
+    echo("<br>");
+    $a=1;
+    $b=1.0;
+    if($a === $b){
+        echo('true');
+    }else{
+        echo('False');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+</head>
+
+<body>
+    <?php
+        print("<h1>hello</h1>");
+    ?>
+</body>
+
+</html>
