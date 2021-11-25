35cdaac9d074bc41e4e78c6748e9a291cb1fa943
wasitakie <94669314+wasitakie@users.noreply.github.com>
2021-11-25 21:47:12 +0700

    basicephphomework

--- variable.php
+++ variable.php
@@ -21,6 +21,17 @@
     }else{
         echo('False');
     }
+    
+$t = date("H");
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
 ?>
 
 <!DOCTYPE html>
--- variable2.php
+++ variable2.php
@@ -1,7 +1,29 @@
 <?php
-$name="wa";
-echo($name);
-    
+    $password= "wa";
+    $name = "wasita";
+    if(($name = "wasita" ) && ($password = "wa")){
+      echo"Login compile";
+    }else{
+      echo"Uncompile";
+    }
+    echo("<br>");
+    $t = date("H");
+    $t = 16;
+    if ($t < "16") {
+        echo "Have a good aternoon!";
+      } elseif ($t < "20") {
+        echo "Have a good day!";
+      } else {
+        echo "Have a good night!";
+      }
+      echo("<br>");
+    $p = 1;
+
+    if($p <= "1"){
+      echo"i love you";
+    }else{
+      echo"i love me";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +31,7 @@ echo($name);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homework</title>
 </head>
 <body>
     
