6d4ac93615682bffe40ae805c7b76bb881291701
wasitakie <94669314+wasitakie@users.noreply.github.com>
2021-12-24 00:21:40 +0700

    homework

---
+++ homework_ps/do while.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dowhile</title>
+</head>
+
+<body>
+    <?php
+    $z=10;
+    do{
+    echo"Nember is $z";
+    $z++;
+    }while($z=10)
+    ?>
+</body>
+
+</html>
---
+++ homework_ps/for loop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>forloop</title>
+</head>
+
+<body>
+    <?php
+    $x="10";
+    for($x=0;$x<=10;$x++){
+        echo"Nember<br>";
+    }
+    ?>
+</body>
+
+</html>
